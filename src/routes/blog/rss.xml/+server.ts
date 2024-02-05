f2f949f775c023a5c7c0661cfcc5af5e1f2b0b00
shilangyu <xmarcinmarcin@gmail.com>
2024-02-05 23:39:13 +0100

    Add rss feed

---
+++ src/routes/blog/rss.xml/+server.ts
@@ -0,0 +1,34 @@
+import { posts } from '$lib/posts';
+
+export const prerender = true;
+
+export async function GET() {
+  const headers = { 'Content-Type': 'application/xml' };
+  const blogUrl = 'https://shilangyu.dev/blog';
+
+  const xml = `
+		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+			<channel>
+				<title>shilangyu.dev</title>
+				<description>Blogging about programming languages, math, or things in between.</description>
+				<link>${blogUrl}</link>
+				<atom:link href="${blogUrl}/rss.xml" rel="self" type="application/rss+xml"/>
+				${posts
+          .map(
+            ({ name, meta }) => `
+						<item>
+							<title>${meta.title}</title>
+							<description>${meta.description}</description>
+							<link>${blogUrl}/${name}</link>
+							<guid isPermaLink="true">${blogUrl}/${name}</guid>
+							<pubDate>${meta.date.toUTCString()}</pubDate>
+						</item>
+					`
+          )
+          .join('')}
+			</channel>
+		</rss>
+	`.trim();
+
+  return new Response(xml, { headers });
+}
