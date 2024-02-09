ca093879341fe111de5d6de62eaf8a0c4219acc5
shilangyu <xmarcinmarcin@gmail.com>
2024-02-09 16:39:26 +0100

    Fix urls

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,13 +1,18 @@
 // I am not aware of any consistent way of getting typed routes or getting a list of routes at all
 // I therefore hardcode things here, hoping this will be improved in the future
 
+import { base } from '$app/paths';
 import type { Post } from './posts';
 
 export const routes = {
-  root: '/',
+  root: base + '/',
   blog: {
-    self: '/blog',
-    post: (post: Post) => `/blog/${post.name}`,
-    rss: `/blog/rss.xml`
+    self: base + '/blog',
+    post: (post: Post) => base + `/blog/${post.name}`,
+    rss: base + `/blog/rss.xml`
   }
 } as const;
+
+export const external = {
+  repository: 'https://github.com/shilangyu/website'
+};
