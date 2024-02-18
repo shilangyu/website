9d130361147129f99c857cd7bab0ebc66c0c3c48
shilangyu <xmarcinmarcin@gmail.com>
2024-02-18 22:04:50 +0100

    Add custom img impl

---
+++ src/lib/mdsvex/img.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let src: string;
+  export let alt: string;
+</script>
+
+<figure>
+  <img {src} {alt} />
+  <figcaption aria-hidden="true">{alt}</figcaption>
+</figure>
+
+<style>
+  figure {
+    display: block;
+    text-align: center;
+    margin: 1em 0;
+  }
+  figure img {
+    border: none;
+    margin: 0 auto;
+    max-width: 100% !important;
+  }
+  figcaption {
+    font-size: 0.8em;
+    font-style: italic;
+    margin: 0 0 0.8em;
+  }
+</style>
---
+++ src/lib/mdsvex/mdsvex.svelte
@@ -0,0 +1,6 @@
+<script lang="ts" context="module">
+  import img from './img.svelte';
+  export { img };
+</script>
+
+<slot />
