2eb64ba3ee1df1a49286ad4c88a7ddef785539d4
shilangyu <xmarcinmarcin@gmail.com>
2024-02-18 22:33:19 +0100

    Add custom blockquotes

---
+++ src/lib/mdsvex/blockquote.svelte
@@ -0,0 +1,13 @@
+<blockquote>
+  <slot />
+</blockquote>
+
+<style>
+  blockquote {
+    border-left: 4px solid var(--color-divider);
+    padding: 0.5em 1em;
+    margin: 1em 0;
+    color: var(--color-text-secondary);
+    border-radius: 0.5rem;
+  }
+</style>
--- src/lib/mdsvex/mdsvex.svelte
+++ src/lib/mdsvex/mdsvex.svelte
@@ -1,6 +1,7 @@
 <script lang="ts" context="module">
+  import blockquote from './blockquote.svelte';
   import img from './img.svelte';
-  export { img };
+  export { blockquote, img };
 </script>
 
 <slot />
