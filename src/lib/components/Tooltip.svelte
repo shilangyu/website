926ba87e2fefac6bd56a79b0c16981d381b3d914
shilangyu <xmarcinmarcin@gmail.com>
2024-03-31 16:35:11 +0200

    Add tooltip component

---
+++ src/lib/components/Tooltip.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  export let text: string;
+</script>
+
+<div class="root">
+  <slot />
+  <span class="tooltip">{text}</span>
+</div>
+
+<style>
+  .root {
+    position: relative;
+  }
+
+  .tooltip {
+    position: absolute;
+    z-index: 1;
+    text-align: center;
+    border-radius: var(--border-radius);
+    padding: 5px;
+    top: -6px;
+    left: 50%;
+    transform: translate(-50%, -100%);
+
+    background-color: var(--color-text-primary);
+    color: var(--color-background);
+
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+
+  .root:hover > .tooltip {
+    visibility: visible;
+    opacity: 1;
+  }
+</style>
