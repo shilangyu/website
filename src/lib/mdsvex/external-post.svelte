62f342c7264bd9b89672066afbf5463ebc4f3bfc
shilangyu <xmarcinmarcin@gmail.com>
2026-06-20 22:35:40 +0200

    Add external post component

---
+++ src/lib/mdsvex/external-post.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  interface Props {
+    url: string;
+  }
+
+  let { url }: Props = $props();
+  let isShowing = $state(false);
+</script>
+
+{#if isShowing}
+  <iframe title="External blog post" src={url}></iframe>
+{:else}
+  The blog post has been posted here: <a href={url} target="_blank" rel="noopener noreferrer"
+    >{url}</a
+  >
+  <br />
+  <button onclick={() => (isShowing = true)}
+    >You can inline the blog post here (please don't)</button
+  >
+{/if}
+
+<style>
+  iframe {
+    border: none;
+    width: 100%;
+    flex: 1;
+    height: 50vh;
+  }
+
+  button {
+    font-size: 0.5em;
+  }
+</style>
